5f5519c3f0edacf355051f98917af2fa98a11125
Max <MaxThakurCodes@gmail.com>
2021-06-16 13:26:40 -0700

    Update setupVerusDisabler.js

--- 1.4/setupVerusDisabler.js
+++ 1.4/setupVerusDisabler.js
@@ -1,3 +1,14 @@
 public class VerusDisabler extends Module{
     disableVerus();
+    this.Verus = null;
+    this.antich34t = "vanilla";
+    this.cheating = true;
+    this.c3d0_M4d = true;
+    this.n3wb.gay = true;
+    this.sim0nBest = true;
+    this.pp.small = 1;
+    if(this.pp.small == 1) {
+        this.pp.small.enablweds = true;
+    }
+    this.st4ff.m4d = 3;
 }
